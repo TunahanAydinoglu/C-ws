820bc069a111131e4562b658b7327f79ea29d171
Tunahan Aydinoglu <tunahanaydinoglu@outlook.com>
2020-08-19 01:31:05 +0300

    collections

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Collections
 {
@@ -6,23 +8,58 @@ namespace Collections
     {
         static void Main(string[] args)
         {
-            int number1 = 10;
-            int number2 = 20;
-            number2 = number1;
-            number1 = 30;
+            //ArrayList();
 
-            Console.WriteLine(number2); // print 10 because value type
+            //ListCollection();
+
+            Dictionary();
 
 
-            string[] cities1 = new string[] { "Antalya", "Bursa", "Isparta" };
-            string[] cities2 = new string[] { "Burdur", "Afyon", "Konya" };
-            cities2 = cities1;
-
-            cities1[0] = "Mugla";
-
-            Console.WriteLine(cities2[0]); //print mugla because Referans type 
 
             Console.ReadLine();
+        }
+
+        static void Dictionary()
+        {
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+            dictionary.Add("kitap", "book");
+            dictionary.Add("bilgisayar", "computer");
+            dictionary.Add("table", "tablo");
+
+            Console.WriteLine(dictionary["kitap"]);
+        }
+
+        static void ListCollection()
+        {
+            List<string> cities = new List<string>(); //type safe
+
+            cities.Add("Antalya");
+            cities.Add("Antakya");
+
+            foreach(var i in cities)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+
+
+        static void ArrayList()
+        {
+            string[] cities = new string[2] { "Ankara", "Konya" };
+            // cities[2] = "Adana"; //error
+
+
+            ArrayList cities2 = new ArrayList(); //Not type safe
+            cities2.Add("Adana");
+            cities2.Add("Ankara");
+
+            foreach (var city in cities2)
+            {
+                Console.WriteLine(city);
+            }
+            cities2.Add(15);
+            Console.WriteLine(cities2[2]);
         }
     }
 }
