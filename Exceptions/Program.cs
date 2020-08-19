65bb6a75730157e2d92d87d5e709b09af1d93a97
Tunahan Aydinoglu <tunahanaydinoglu@outlook.com>
2020-08-19 12:28:57 +0300

    Exceptions

---
+++ Exceptions/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string[] students = new string[3] { "Tunahan", "Furkan", "Ramazan" };
+
+                students[3] = "Mehmet"; // error
+            }
+            catch (DivideByZeroException excep)
+            {
+                Console.WriteLine(excep.Message);
+            }
+            catch (IndexOutOfRangeException excep)
+            {
+                Console.WriteLine(excep.Message);
+            }
+            catch (Exception exception)
+            {
+
+                Console.WriteLine("Koddaki hata : {0}",exception.Message);
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
