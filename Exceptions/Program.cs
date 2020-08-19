130db5da3c6a5592e96c5db3557af591e1c56456
Tunahan Aydinoglu <tunahanaydinoglu@outlook.com>
2020-08-19 12:44:18 +0300

    CustomException

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -1,10 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Exceptions
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            //ExceptionIntro();
+
+
+            
+            try
+            {
+                CustomException();
+            }
+            catch (RecordNotFoundException exception)
+            {
+
+                Console.WriteLine(exception.Message);
+            }
+
+
+
+
+            Console.ReadLine();
+        }
+
+        private static void CustomException()
+        {
+            List<string> students = new List<string> { "Tunahan", "Kerem", "Bilal" };
+
+            if (!students.Contains("Ahmet"))
+            {
+                throw new RecordNotFoundException("Record Not Found!");
+            }
+            else
+            {
+                Console.WriteLine("Record Found!");
+            }
+        }
+
+        private static void ExceptionIntro()
         {
             try
             {
@@ -23,11 +60,9 @@ namespace Exceptions
             catch (Exception exception)
             {
 
-                Console.WriteLine("Koddaki hata : {0}",exception.Message);
+                Console.WriteLine("Koddaki hata : {0}", exception.Message);
 
             }
-
-            Console.ReadLine();
         }
     }
 }
