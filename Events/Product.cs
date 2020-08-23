065d362df718b6f57e7f9c098efcf63ca37af227
Tunahan Aydinoglu <tunahanaydinoglu@outlook.com>
2020-08-24 02:40:57 +0300

    Events

---
+++ Events/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Events
+{
+    public delegate void StockControl();
+    public class Product
+    {
+        int _stock;
+        public Product(int stock)
+        {
+            _stock = stock;
+        }
+        public event StockControl StockControlEvent;
+        public string ProductName { get; set; }
+        public int Stock { get { return _stock; }set { _stock = value;
+            if(value <= 15 && StockControlEvent != null)
+                {
+                    StockControlEvent();
+                }
+                    } }
+
+        public void Sell(int amount)
+        {
+            Stock -= amount;
+            Console.WriteLine("Stock Amount : {0}", Stock);
+        }
+    }
+}
---
+++ Events/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product hdd = new Product(50);
+            hdd.ProductName = "HardDisk";
+
+            Product gsm = new Product(50);
+            gsm.StockControlEvent += Gsm_StockControlEvent;
+
+            for(int i=0; i<10; i++)
+            {
+                hdd.Sell(10);
+                gsm.Sell(10);
+                Console.ReadLine();
+            }
+        }
+
+        private static void Gsm_StockControlEvent()
+        {
+            Console.WriteLine("Stock Check pls");
+        }
+    }
+
+}
